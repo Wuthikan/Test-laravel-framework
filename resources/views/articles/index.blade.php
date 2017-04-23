f26237aa0ac1aba1ee51d0c098c54152be231721
Wuthikan <icewoottikarn@gmail.com>
2017-04-24 01:32:07 +0700

    edit two

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -22,9 +22,7 @@
 		<div class="panel panel-default">
 			<div class="panel-heading">
 
-				<a href="{{ url('articles/'. $article->id) }}">
-				   {{ $article->title }}
-				</a>
+			
 
 			</div>
 			<div class="panel-body">{{ $article->body }}</div>
